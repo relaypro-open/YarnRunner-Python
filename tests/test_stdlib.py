991f3b1cdbc956bcd8177771d3943b5c53a0b3c6
Jose Falanga <jose.falanga@gmail.com>
2023-06-30 22:09:33 -0300

    format

--- tests/test_stdlib.py
+++ tests/test_stdlib.py
@@ -2,13 +2,17 @@ import os
 from .context import YarnRunner
 
 compiled_yarn = open(os.path.join(os.path.dirname(
-    __file__), '../examples/yarn2/standart-lib.yarnc'), 'rb')
+    __file__), '../examples/yarn2/stdlib.yarnc'), 'rb')
 names_csv = open(os.path.join(os.path.dirname(
-    __file__), '../examples/yarn2/standart-lib.csv'), 'r')
+    __file__), '../examples/yarn2/stdlib.csv'), 'r')
 
 runner = YarnRunner(compiled_yarn, names_csv, autostart=False)
 
 
 def test_variables2():
     runner.resume()
+
+    # we are just happy nothing exploded while running
+    # that means every stdlib function is invoked correctly
+    # if this wasn't the case, we would have an exception
     assert runner.finished
